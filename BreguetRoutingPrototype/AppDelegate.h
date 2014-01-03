15c7b9df3d5869dd3ae3efef9aea534525a1abf8
Marius Gächter <marius.gaechter@leafit.ch>
2014-01-03 10:46:33 +0100

    Initial Commit

---
+++ BreguetRoutingPrototype/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  BreguetRoutingPrototype
+//
+//  Created by Marius Gächter on 03.01.14.
+//  Copyright (c) 2014 leafit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BreguetRoutingPrototype/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BreguetRoutingPrototype
+//
+//  Created by Marius Gächter on 03.01.14.
+//  Copyright (c) 2014 leafit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
